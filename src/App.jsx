22f3015fd136e0724044cc77a8df625fedd50384
yggarcia <garciayuni84@gmail.com>
2023-12-20 11:39:26 -0500

    Add derive state for GameBoard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,44 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import {useState} from "react";
 import Log from "./components/Log";
+import {act} from "react-dom/test-utils";
 
 function App() {
     const [activePlayer, setActivePlayer] = useState('X');
     const [gameTurns, setGamesTurns] = useState([]);
 
-    function handleSelectSquare() {
+    function handleSelectSquare(rowIdx, colIdx) {
         setActivePlayer((prevState) => {
-            return prevState === 'X' ? 'O' : 'X'});
+            return prevState === 'X' ? 'O' : 'X'
+        });
+
+        setGamesTurns((prevTurns) => {
+            let currentPlayer = 'X'
+
+            if (prevTurns.length && prevTurns[0].player === 'X') {
+                currentPlayer = 'O'
+            }
+
+            const updatedTurns = [
+                {
+                    square: {row: rowIdx, col: colIdx},
+                    player: currentPlayer
+                }, ...prevTurns];
+
+            return updatedTurns;
+        })
     }
 
     return (<main>
-            <div id="game-container">
-                <ol id="players" className={'highlight-player'}>
-                <Player initialName="Player one" symbol='X' isActive={activePlayer === 'X'} ></Player>
+        <div id="game-container">
+            <ol id="players" className={'highlight-player'}>
+                <Player initialName="Player one" symbol='X' isActive={activePlayer === 'X'}></Player>
                 <Player initialName="Player two" symbol='O' isActive={activePlayer === 'O'}></Player>
-                </ol>
-                <GameBoard onSelectSquare={()=> {handleSelectSquare()}} activePlayer={activePlayer}/>
-            </div>
+            </ol>
+            <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        </div>
         <Log/>
-        </main>)
+    </main>)
 }
 
 export default App
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,24 +6,33 @@ const initialBoard = [
     [null, null, null],
 ]
 
-export default function GameBoard( {onSelectSquare, activePlayer}) {
-    const [gameBoard, setGameBoard] = useState(initialBoard);
+export default function GameBoard( {onSelectSquare, turns}) {
+    //todo: we uplifted the state to app.js and this is a derived state from app.js' state
+    let gameBoard = initialBoard;
 
-    function handleSelectSquare(row, col) {
-        // WRONG: see bellow
-        // setGameBoard((prevGameBoar) => {
-        //     prevGameBoar[row][col] = 'X';
-        //     return prevGameBoar;
-        // })
-        //todo: if the state is an object or array you should update immutably by creating a copy of that obj.
-        setGameBoard((prevGameBoard) => {
-            // todo: deep copy of the by-dimensional array
-            const copyOfPrevGameBoard = [...prevGameBoard.map(row => [...row])];
-            copyOfPrevGameBoard[row][col] = activePlayer;
-            return copyOfPrevGameBoard;
-        })
-        onSelectSquare();
+    for (let turn of turns) {
+        const {player, square} = turn;
+        const {row, col } = square;
+
+        gameBoard[row][col] = player;
     }
+    // const [gameBoard, setGameBoard] = useState(initialBoard);
+    //
+    // function handleSelectSquare(row, col) {
+    //     // WRONG: see bellow
+    //     // setGameBoard((prevGameBoar) => {
+    //     //     prevGameBoar[row][col] = 'X';
+    //     //     return prevGameBoar;
+    //     // })
+    //     //todo: if the state is an object or array you should update immutably by creating a copy of that obj.
+    //     setGameBoard((prevGameBoard) => {
+    //         // todo: deep copy of the by-dimensional array
+    //         const copyOfPrevGameBoard = [...prevGameBoard.map(row => [...row])];
+    //         copyOfPrevGameBoard[row][col] = activePlayer;
+    //         return copyOfPrevGameBoard;
+    //     })
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id={'game-board'}>
@@ -32,7 +41,7 @@ export default function GameBoard( {onSelectSquare, activePlayer}) {
                     <ol>
                         {row.map((playerSymbol, colIdx) => {
                             return <li key={colIdx}>
-                                <button onClick={() => {handleSelectSquare(rowIndex, colIdx)}}>{playerSymbol}</button></li>
+                                <button onClick={() => onSelectSquare(rowIndex, colIdx)}>{playerSymbol}</button></li>
                         })}
                     </ol>
                 </li>
